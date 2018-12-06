ea6a1005d247d478f7f12644d63eb7e1d3131f8f
Tchea JiaWei <tcheajiawei@hotmail.com>
2018-12-06 15:15:14 +0800

    Remove redunctant database connection checking

--- dbfunction.php
+++ dbfunction.php
@@ -8,15 +8,6 @@ function getDbConnect() {
     $con =  mysqli_connect("localhost:3306", "waduser01", "Password4321", "myartspacedb");
     return $con;
 }
-/*
-$con= getDbConnect();
-
-if (!mysqli_connect_error($con)) {
-    echo "success!"; 
-} else { 
-    echo "fail!";
-    
-}*/
-        
+  
 
 ?>
